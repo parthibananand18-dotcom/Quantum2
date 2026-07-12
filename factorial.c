ae25d13ae93dec8bc1257b89a53cdb7f8e1f149f
Parthiban <parthibananand18@gmail.com>
2026-07-12 17:32:54 +0530

    Implement factorial calculation in factorial.c
    Fix the logic for calculating factorial and correct variable names.

---
+++ factorial.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int n,i=1,fact=1;
+  printf("Enter the value of n: ");
+  scanf("%d",&n);
+  while(i<=1)
+    {
+      Fact=fact*i;
+      i++;
+    }
+  printf("Factorial of natural numbers: %d",Fact);
+  return 0;
+}
