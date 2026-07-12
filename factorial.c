8d2671c37ea8275bb3e1bd05380f214c8291546f
parthibananand18-dotcom <parthibananand18@gmail.com>
2026-07-12 18:31:18 +0530

    program modified using do while

--- factorial.c
+++ factorial.c
@@ -1,14 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-  int n,i=1,fact=1;
+  int n, i = 1, fact = 1;
   printf("Enter the value of n: ");
-  scanf("%d",&n);
-  while(i<=1)
-    {
-      Fact=fact*i;
-      i++;
-    }
-  printf("Factorial of natural numbers: %d",Fact);
+  scanf("%d", &n);
+  do
+  {
+    fact = fact * i;
+    i++;
+  }
+   while (i <= n);
+  printf("Factorial of natural numbers: %d", fact);
   return 0;
 }
