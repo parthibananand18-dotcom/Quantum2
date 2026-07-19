726563edd06138d48d63db8d2d905ac90beac9ef
Parthiban <parthibananand18@gmail.com>
2026-07-19 18:11:11 +0530

    Implement number reversal in ReversedNumber.c
    Fixes the logic for reversing a number and includes input prompts.

---
+++ ReversedNumber.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int num,rev=0;
+  printf("Enter the value of num: ");
+  scanf("%d",&num);
+  while(n>0)
+    {
+      rev=rev*10;
+      rev=rev+(num%10);
+      num=num/10;
+    }
+  printf("Reverse of the given number:%d",rev);
+  return 0;
+}
